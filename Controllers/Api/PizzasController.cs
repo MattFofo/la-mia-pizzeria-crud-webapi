e40656f3449efc3374f42a1b81cd33191e78a9e3
MattFofo <matteo.segattini2@gmail.com>
2022-07-26 13:52:32 +0200

    creato api controller per le pizze

---
+++ Controllers/Api/PizzasController.cs
@@ -0,0 +1,24 @@
+﻿using la_mia_pizzeria.DataBase;
+using la_mia_pizzeria.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace la_mia_pizzeria.Controllers.Api
+{
+    [Route("api/pizzas")]
+    [ApiController]
+    public class PizzasController : ControllerBase
+    {
+
+        public ActionResult Get()
+        {
+
+            PizzeriaContext ctx = new PizzeriaContext();
+
+            IQueryable<Pizza> pizzasList = ctx.Pizzas;
+
+            return Ok(pizzasList.ToList());
+  
+        }
+    }
+}
