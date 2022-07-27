70133a2a944edad1e178fb37efb2312317d4ed03
MattFofo <matteo.segattini2@gmail.com>
2022-07-27 12:44:45 +0200

    implementata ricerca pizza tramite chiamata api nell'index home

--- Controllers/Api/PizzasController.cs
+++ Controllers/Api/PizzasController.cs
@@ -10,12 +10,19 @@ namespace la_mia_pizzeria.Controllers.Api
     public class PizzasController : ControllerBase
     {
 
-        public ActionResult Get()
+        public ActionResult Get(string? search)
         {
 
             PizzeriaContext ctx = new PizzeriaContext();
 
             IQueryable<Pizza> pizzasList = ctx.Pizzas;
+
+            if(search != null && search != "")
+            {
+                pizzasList = ctx.Pizzas.Where(p => p.Name.Contains(search));
+            }
+            
+            
 
             return Ok(pizzasList.ToList());
   
